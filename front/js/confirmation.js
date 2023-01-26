5da5a8316d966c4afb057473116cc1ddabec87e6
Thomas <86045709+ThomasBrl@users.noreply.github.com>
2023-01-26 16:38:28 +0100

    Update confirmation.js
    Ajout des commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,7 @@
+// La fonction main() définit une variable "orderId" qui récupère la valeur de l'identifiant de 
+// commande passé dans l'URL. Il utilise ensuite la propriété "searchParams" URL pour
+// récupérer la valeur de "orderId" dans l'URL
+// Il utilise ensuite la propriété innerText de l'élément HTML ayant l'id "orderId" pour afficher cette valeur.
 function main() {
   var str = window.location.href;
   var url = new URL(str);
